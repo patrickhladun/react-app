fead22aaff2885290abbcb55339951249d262bef
Patrick <patrickhladun@gmail.com>
2021-06-09 16:49:28 +0100

    Add router

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import NotFound from './pages/NotFound';
 
-const App = () => {
-    return(
-        <Fragment>
-            <h1>Node API User Authentication</h1>
-            <Dashboard />
-        </Fragment>
-    );
+class App extends Component {
+    
+    render() {
+        return(
+            <Fragment>
+                <h1>Node API User Authentication</h1>
+                <ul>
+                    <li><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></li>
+                    <li><NavLink to="/login" activeClassName="is-active" exact={true}>Login</NavLink></li>
+                    <li><NavLink to="/signup" activeClassName="is-active" exact={true}>Signup</NavLink></li>
+                </ul>
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={Signup} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const { merge } = require('webpack-merge');
 const config = require('./webpack.config');
 
 module.exports = merge(config, {
+    mode: 'development',
     module: {
         rules: [
             {
